512360af326b9fc054f16351721b9e918ceb8b6d
Suman <sumanbhowmik.biz@gmail.com>
2024-07-24 15:53:54 +0530

    Git Commit by American

--- src/test/java/qaclickacademy/com/RESTAPITest.java
+++ src/test/java/qaclickacademy/com/RESTAPITest.java
@@ -10,6 +10,13 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		
+//		Added by GitX master
+//		Added by GitX master
+		
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
 	}
 	
 	@Test
